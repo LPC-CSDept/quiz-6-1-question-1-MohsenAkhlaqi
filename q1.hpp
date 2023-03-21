cb0b4e4eee375e40b0135cd70b20b3d4d7976dc8
Mohsen <mmakhlaqi@gmail.com>
2023-03-21 14:56:06 -0700

    work

--- q1.hpp
+++ q1.hpp
@@ -1,6 +1,3 @@
-#include <iostream>
-#include <iomanip>
-using namespace std;
 
 //********************
 // Declare Function Prototypes
@@ -9,3 +6,45 @@ using namespace std;
 // ******************************
 // Implement all your functions here
 // ******************************
+
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void swapTwoValues(int n1, int n2) {
+    int temp = n1;
+    n1 = n2;
+    n2 = temp;
+}
+
+void swapTwoValues(float f1, float f2) {
+    float temp = f1;
+    f1 = f2;
+    f2 = temp;
+}
+
+void getinput(int& n1, int& n2) {
+    cout << "Enter two integers: ";
+    cin >> n1 >> n2;
+}
+
+void getinput(float& f1, float& f2) {
+    cout << "Enter two floats: ";
+    cin >> f1 >> f2;
+}
+
+int main() {
+    int n1, n2;
+    getinput(n1, n2);
+    swapTwoValues(n1, n2);
+    cout << "After swapping: " << n1 << " " << n2 << endl;
+
+    float f1, f2;
+    getinput(f1, f2);
+    swapTwoValues(f1, f2);
+    cout << "After swapping: " << fixed << setprecision(2) << f1 << " " << f2 << endl;
+
+    return 0;
+}
